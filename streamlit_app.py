0d2d83df5875ffc37a8259c669628f5e563652a4
thetomarchhavi <chhavitomar47@gmail.com>
2024-05-02 02:38:45 +0530

    chatbot

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,11 @@ import replicate
 import os
 
 # App title
-st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
+st.set_page_config(page_title="OINK🐷")
 
 # Replicate Credentials
 with st.sidebar:
-    st.title('🦙💬 Llama 2 Chatbot')
+    st.title('OINK🐷')
     st.write('This chatbot is created using the open-source Llama 2 LLM model from Meta.')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         st.success('API key already provided!', icon='✅')
